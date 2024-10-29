a1ac64456a73812074a2e271a53e56e0edb58fc0
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-10-29 06:55:08 +0300

    Fixed source leak problem

--- Command Line Interpreter/src/Main.java
+++ Command Line Interpreter/src/Main.java
@@ -6,14 +6,15 @@ public class Main {
     public static void main(String[] args) {
         CMD Cmd = new CMD();
         String Command;
-        while (true) {
-            Scanner input = new Scanner(System.in);
-            System.out.print("Enter command or 'quit' to exit: ");
-            Command = input.nextLine();
-            System.out.println(Cmd.execute(Command));
-            if (Command.equals("quit")) {
-                input.close();
-                break;
+        try (Scanner input = new Scanner(System.in)) {
+            while (true) {
+                System.out.print("Enter command or 'quit' to exit: ");
+                Command = input.nextLine();
+                System.out.println(Cmd.execute(Command));
+                if (Command.equals("quit")) {
+                    input.close();
+                    break;
+                }
             }
         }
     }
